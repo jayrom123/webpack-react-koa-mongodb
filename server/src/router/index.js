301dcc2e8c972463198f53ea5a93afd7d920604e
luo.wenjie@linkingmed.com <luo.wenjie@linkingmed.com>
2017-10-18 18:57:21 +0800

    change packge.json

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -7,6 +7,10 @@ let home = require('./home.js');
 
 let route = new Router();
 
+route.get('/',async (ctx,next)=>{
+  ctx.body = "idnex_page";
+});
 route.use("/home",home.routes(),home.allowedMethods());
 
 module.exports = route;
+
